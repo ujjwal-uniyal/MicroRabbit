57ada69bb48349b61d3edd3c90d051769ceeb147
Ujjwal Uniyal <Ujjwal.uniyal111@gmail.com>
2020-10-26 11:17:08 +0530

    Implemented Interfaces and Classes for Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,18 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }  // only those who inherit from this class should be able to set the timestamp
+
+        protected Command()
+        {
+            Timestamp = DateTime.UtcNow;
+        }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+   public abstract class Message : IRequest<bool>
+    {
+        public string MessageType { get;protected set; }
+     
+
+        public Message()
+        {
+            MessageType = GetType().Name;
+           
+        }
+    }
+}
